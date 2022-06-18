9f863eb5d66eb8fece2cc742b18dff65fe53923f
huyldth1908041 <huyldth1908041>
2022-06-18 10:16:00 +0700

    config bsc test

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,9 @@ const providerOptions = {
     options: {
       infuraId: "14b979bc481c4c71bc2116ac116e2675", // required
       rpc: {
-        97: 'https://data-seed-prebsc-2-s1.binance.org:8545/',
-        56: 'https://bsc-dataseed.binance.org/'
+        97: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
       },
       chainId: 97,
-      network: 'binance'
     },
     display: {
       name: 'WalletConnect',
