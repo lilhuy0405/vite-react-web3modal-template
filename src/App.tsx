b5c23b608f2ebe8c44ea4ad68fda46f5b1aaec13
huyldth1908041 <huyldth1908041>
2022-06-19 22:57:20 +0700

    complete template

--- src/App.tsx
+++ src/App.tsx
@@ -1,132 +1,26 @@
-import {useEffect, useState} from 'react'
-import Web3Modal from "web3modal";
-import {ethers} from "ethers";
-import WalletConnectProvider from "@walletconnect/web3-provider";
-import {toast, Toaster} from "react-hot-toast";
-
-const providerOptions = {
-  walletconnect: {
-    package: WalletConnectProvider, // required
-    options: {
-      infuraId: "14b979bc481c4c71bc2116ac116e2675", // required
-      rpc: {
-        97: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
-      },
-      chainId: 97,
-    },
-    display: {
-      name: 'WalletConnect',
-    }
-  },
-  injected: {
-    package: null,
-    name: 'Injected',
-  }
-};
-const web3Modal = new Web3Modal({
-  providerOptions, // required
-  cacheProvider: true,
-  disableInjectedProvider: false,
-
-
-});
+import {Toaster} from "react-hot-toast";
+import updater from "./updater";
+import useWeb3 from "./hooks/useWeb3";
+import {useConnection} from "./states/connection";
 
 function App() {
-  //redux
-  const [web3Instance, setWeb3Instance] = useState<any>(null)
-  const [provider, setProvider] = useState<ethers.providers.Web3Provider | null>(null)
-  const [connection, setConnection] = useState<{ address: string, chainId: number }>({address: '', chainId: -1})
-  const [event, setEvent] = useState('')
-
-  //hooks
-  const switchChain = async (web3Instance: any) => {
-    try {
-      console.log("Switch Chain")
-      // this wallet_addEthereumChain request only run if the requested chain is not installed
-      await web3Instance.request({
-        method: 'wallet_addEthereumChain',
-        params: [
-          {
-            chainId: "0x61",
-            rpcUrls: ["https://data-seed-prebsc-1-s1.binance.org:8545/"],
-            chainName: "Smart Chain - Testnet",
-            // these properties below is optional
-            nativeCurrency: {
-              name: 'tBNB',
-              symbol: 'tBNB', // 2-6 characters long
-              decimals: 18
-            },
-            blockExplorerUrls: ['https://testnet.bscscan.com/']
-          },
-        ],
-      });
-      // this wallet_switchEthereumChain only run if current chain is not the chain specified
-      await web3Instance.request({
-        method: 'wallet_switchEthereumChain',
-        params: [{chainId: '0x61'}], // chainId must be in hexadecimal numbers
-      });
-      return true
-    } catch (err: any) {
-      console.log(err)
-      return false
-    }
-  }
-  const handleConnect = async () => {
-    const instance = await web3Modal.connect();
-    const isSwitchSuccess = switchChain(instance);
-    if (!isSwitchSuccess) return
-    const provider = new ethers.providers.Web3Provider(instance);
-    const netWork = await provider.getNetwork();
-    const signer = provider.getSigner();
-    const address = await signer.getAddress();
-    setWeb3Instance(instance)
-    setConnection({
-      chainId: netWork.chainId,
-      address
-    })
-    setProvider(provider);
-  }
-
-  const handleChangeChain = () => {
-    console.log('chain changed')
-    setEvent('net work changed')
-    toast.success('network changed')
-  }
-
-  const handleAccountChain = () => {
-    console.log('account changed')
-    setEvent('account changed')
-    toast.success('account changed')
-  }
-  //subcribe for events use effect should in updater.tsx
-  useEffect(() => {
-    console.log('subscribe to EIP-1193 events with web3Instance: ' + web3Instance)
-    if (web3Instance) {
-      web3Instance.on('accountsChanged', handleAccountChain)
-      web3Instance.on('chainChanged', handleChangeChain)
-    }
-  }, [web3Instance])
-
-
-  useEffect(() => {
-    console.log('call connect-to-cached-provider useEffect with cached: ' + web3Modal.cachedProvider)
-    if (web3Modal.cachedProvider) {
-      handleConnect();
-    }
-  }, [web3Modal.cachedProvider])
-
+  //example to use useWeb3() hook
+  const {isActive, activate, deActivate} = useWeb3()
+  const {connection} = useConnection()
   return (
-    <div className="App">
-      <button onClick={handleConnect}>Open modal</button>
+    <div>
+      <h3>A Template to connect multi blockchains wallets check current network, save connection state to redux,..</h3>
       <div>
-        connected to network id: {connection.chainId} with account: {connection.address}
+        <button onClick={isActive ? deActivate : activate}>
+          {isActive ? 'Log out wallet' : 'Connect wallet'}
+        </button>
       </div>
       <div>
-        events trigger: {event}
+        wallet address: {connection.address}
       </div>
       <Toaster/>
     </div>
   )
 }
 
-export default App
+export default updater(App)
---
+++ src/constants/index.ts
@@ -0,0 +1,34 @@
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import Web3Modal from "web3modal";
+
+
+export const SMARTCHAIN_TESTNET_CHAIN_ID = '0x61';
+export const SMARTCHAIN_TESTNET_CHAIN_ID_DECIMAL = 97;
+export const SMARTCHAIN_TESTNET_URL = "https://data-seed-prebsc-1-s1.binance.org:8545/"
+
+const providerOptions = {
+  walletconnect: {
+    package: WalletConnectProvider, // required
+    options: {
+      infuraId: import.meta.env.VITE_INFURA_ID, // required
+      rpc: {
+        [SMARTCHAIN_TESTNET_CHAIN_ID_DECIMAL]: SMARTCHAIN_TESTNET_URL,
+      },
+      chainId: SMARTCHAIN_TESTNET_CHAIN_ID_DECIMAL,
+    },
+    display: {
+      name: 'WalletConnect',
+    }
+  },
+  injected: {
+    package: null,
+    name: 'Injected',
+  }
+};
+export const web3Modal = new Web3Modal({
+  providerOptions, // required
+  cacheProvider: true,
+  disableInjectedProvider: false,
+  theme: 'dark'
+
+});
